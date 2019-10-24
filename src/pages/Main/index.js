89eada16759bf7d6c06e1d9e9e71eaf91e1bf3df
André Brandão <andrenbrandao@gmail.com>
2019-10-24 18:18:39 -0300

    Add repository details page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-import { Container, Form, List, SubmitButton } from './styles';
+import { Form, List, SubmitButton } from './styles';
+import Container from '../../components/Container';
 
 export default class Main extends Component {
   state = {
@@ -83,7 +85,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
